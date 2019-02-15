6b32128aea53e2df75fbe74a84348d5d8a2271ec
pepe_witch <skpepe134@gmail.com>
2019-02-15 16:01:22 +0700

    create chat component

---
+++ src/components/ChatBox.tsx
@@ -0,0 +1,72 @@
+import React, { useState } from 'react';
+import styles from './ChatBox.module.css';
+import { Message } from '../types/Chat';
+import { ReactComponent as Send } from '../assets/icons/baseline-send-24px.svg';
+import ChatService from '../services/ChatService';
+
+interface TextBoxProps {
+  messages: Message[];
+}
+
+const TypingBox = () => {
+  const [text, setText] = useState('');
+  const send = () => {
+    console.log(text.length);
+    ChatService.send(text);
+    setText('');
+  };
+  return (
+    <form
+      className={styles.typing_container}
+      onSubmit={e => {
+        e.preventDefault();
+        send();
+      }}
+    >
+      <input
+        value={text}
+        onChange={e => setText(e.target.value)}
+        className={styles.text_input}
+      />
+      <Send
+        width={24}
+        height={24}
+        className={styles.send}
+        onClick={() => send()}
+      />
+    </form>
+  );
+};
+
+const TextBox = (props: TextBoxProps) => {
+  const { messages } = props;
+  return (
+    <div className={styles.container}>
+      {messages.map((message, index) => (
+        <div
+          key={`${index}${message.timestamp}`}
+          className={styles.text_container}
+        >
+          <span className={styles.sender}> {message.sender} :</span>{' '}
+          <span>{message.text}</span>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+const ChatBox = () => {
+  const messages = [];
+  for (let i = 0; i < 20; i++) {
+    messages.push({ sender: 'sender', text: 'text', timestamp: new Date() });
+  }
+
+  return (
+    <div className={styles.container}>
+      <TextBox messages={messages} />
+      <TypingBox />
+    </div>
+  );
+};
+
+export default ChatBox;
